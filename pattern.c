23e4924c2fb7e4cbb21c3d681af7c158374fadb4
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-01-15 21:42:57 -0500

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+void main( )
+{
+ int i,space,rows,k=0;
+clrscr( );
+ printf("Enter the number of rows: ");
+ scanf("%d",&rows);
+ for(i=1;i<=rows;++i)
+ {
+ for(space=1;space<=rows-i;++space)
+ {
+ printf(" ");
+ }
+ while(k!=2*i-1)
+ {
+ printf("* ");
+ ++k;
+ }
+ k=0;
+ printf("\n");
+ }
+ getch( );
+}
