d7e60772ab6b95f005a8e1a2f6ec709dce78e46c
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-01-15 21:43:50 -0500

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main( )
+{
+int a;
+clrscr();
+printf("\n enter the number ");
+scanf("%d",&a);
+if(a%2==0)
+printf("\n number is even ");
+else
+printf("\n number is odd ");
+getch( );
+}
